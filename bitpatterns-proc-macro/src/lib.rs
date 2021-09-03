30f5836867bd7dd7d76dfc652bda478225baf0f1
zachmatson <zacharywdmatson@gmail.com>
2021-09-03 15:26:00 -0400

    Make proc-macro failures result in emitting a compiler_error!() instead of panicking

--- bitpatterns-proc-macro/src/lib.rs
+++ bitpatterns-proc-macro/src/lib.rs
@@ -17,7 +17,10 @@ use std::str::FromStr;
 #[doc(hidden)]
 #[proc_macro]
 pub fn __bitpattern_inner(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    __bitpattern_testable(input.into()).into()
+    match __bitpattern_testable(input.into()) {
+        Ok(result) => result.into(),
+        Err(msg) => format!("compile_error!(\"{}\")", msg).parse().unwrap(),
+    }
 }
 
 #[cfg(not(test))]
@@ -27,7 +30,7 @@ use proc_macro2::*;
 
 /// Another inner layer under our proc macro which can be tested and will use `proc_macro` for normal use
 ///  or `proc_macro2` for testing
-fn __bitpattern_testable(input: TokenStream) -> TokenStream {
+fn __bitpattern_testable(input: TokenStream) -> Result<TokenStream, String> {
     // Input is a TokenStream consisting of:
     // - The crate prefix identifier ($crate)
     // - A pattern string literal
@@ -48,9 +51,9 @@ fn __bitpattern_testable(input: TokenStream) -> TokenStream {
     // We will require the literal here to be a string
     let pattern = pattern_literal
         .strip_prefix('"')
-        .expect(PARSE_FAILED_MSG)
+        .ok_or_else(|| LITERAL_WRONG_TYPE_MSG.to_owned())?
         .strip_suffix('"')
-        .expect(PARSE_FAILED_MSG);
+        .ok_or_else(|| LITERAL_WRONG_TYPE_MSG.to_owned())?;
 
     // Support optional 0b prefix and _ separator
     let pattern = pattern
@@ -62,12 +65,12 @@ fn __bitpattern_testable(input: TokenStream) -> TokenStream {
         '1' => Some('1'),
         '0' | '.' => Some('0'),
         _ => None,
-    });
+    })?;
     let cleared = map_bitstring(&pattern, |c| match c {
         '0' => Some('1'),
         '1' | '.' => Some('0'),
         _ => None,
-    });
+    })?;
 
     let size = [8, set.len(), cleared.len()]
         .iter()
@@ -75,16 +78,19 @@ fn __bitpattern_testable(input: TokenStream) -> TokenStream {
         .unwrap()
         .next_power_of_two();
 
-    assert!(size <= 128, "{}", PATTERN_TOO_LARGE_MSG);
+    if size > 128 {
+        return Err(PATTERN_TOO_LARGE_MSG.to_owned());
+    }
 
-    let type_suffix = declared_type.map_or_else(
-        || format!("u{}", size),
-        |t| match get_primitive_int_size(&t) {
-            // The type must be *large enough* to hold the pattern
+    let type_suffix = match declared_type {
+        None => format!("u{}", size),
+        Some(t) => match get_primitive_int_size(&t) {
             Some(declared_size) if declared_size >= size => t,
-            _ => panic_invalid_type_size(size),
+            _ => {
+                return Err(type_size_invalid_msg(size));
+            }
         },
-    );
+    };
 
     // We don't need to worry about preserving the span because the output of this will be wrapped in another macro
     // Crate prefix must be added separately because we get the special `$crate` ident from the declarative macro,
@@ -98,12 +104,12 @@ fn __bitpattern_testable(input: TokenStream) -> TokenStream {
     .parse()
     .expect(PARSE_FAILED_MSG);
 
-    vec![
+    Ok(vec![
         crate_prefix,
         TokenTree::Group(Group::new(Delimiter::None, output_fragment)),
     ]
     .into_iter()
-    .collect()
+    .collect())
 }
 
 /// Extract the stream from a TokenTree group
@@ -123,22 +129,22 @@ fn flatten_tokenstream(input: TokenStream) -> Vec<TokenTree> {
 
 /// Map the characters of a bitstring using a mapper function and panic with appropriate message if some characters
 /// cannot be mapped. `_` characters are ignored.
-fn map_bitstring<F>(bitstring: &str, mapper: F) -> String
+fn map_bitstring<F>(bitstring: &str, mapper: F) -> Result<String, String>
 where
     F: Fn(char) -> Option<char>,
 {
     let result: String = bitstring
         .chars()
-        .map(|c| mapper(c).unwrap_or_else(|| panic_invalid_num_char(bitstring, c)))
+        .map(|c| mapper(c).ok_or_else(|| num_char_invalid_msg(bitstring, c)))
         // Skips *leading* zeros in the mapped string
         // This might allow us to optimize the size of a pattern with leading ignored bits
-        .skip_while(|c| *c == '0')
-        .collect();
+        .skip_while(|c| c.as_ref().map(|c| *c == '0').unwrap_or(false))
+        .collect::<Result<String, String>>()?;
 
-    match result.len() {
+    Ok(match result.len() {
         0 => "0".to_string(),
         _ => result,
-    }
+    })
 }
 
 /// Get the size of a primitive int type given the name, or return `None` if the input isn't of the form `[ui](\d+)`
@@ -148,15 +154,18 @@ fn get_primitive_int_size(type_name: &str) -> Option<usize> {
 
 /// Failure message for generic parse failure
 const PARSE_FAILED_MSG: &str = "Failed to parse bitpattern! arguments.";
+/// Failure message for wrong literal type
+const LITERAL_WRONG_TYPE_MSG: &str =
+    "The pattern argument for bitpattern! must be a string literal";
 /// Failure message for pattern that is too large
 const PATTERN_TOO_LARGE_MSG: &str = "A pattern cannot have a size greater than 128 bits.";
 /// Failure message for invalid or too small type
-fn panic_invalid_type_size(required_size: usize) -> ! {
-    panic!("Explicit type given for bitpattern! is invalid or too small to fit the pattern. The pattern requires a type with at least {} bits.", required_size)
+fn type_size_invalid_msg(required_size: usize) -> String {
+    format!("Explicit type given for bitpattern! is invalid or too small to fit the pattern. The pattern requires a type with at least {} bits.", required_size)
 }
 /// Failure message for failure to parse the pattern as numbers
-fn panic_invalid_num_char(pattern: &str, invalid_char: char) -> ! {
-    panic!(
+fn num_char_invalid_msg(pattern: &str, invalid_char: char) -> String {
+    format!(
         "Failed to parse pattern {} as a number due to the invalid character {}. The pattern should contain the digits '0' and '1', with '.' used for a wildcard.",
         pattern, invalid_char
     )
@@ -164,34 +173,51 @@ fn panic_invalid_num_char(pattern: &str, invalid_char: char) -> ! {
 
 #[cfg(test)]
 mod tests {
-    use std::panic::catch_unwind;
+    // These tests focus on whether a macro fails, succeeds, or panics.
+    // The correctness of the output is tested in the main bitpatterns crate,
+    // but cases where the input would fail compilation have to be tested here.
 
     use quote::quote;
 
     use crate::__bitpattern_testable;
-    use PanicStatus::*;
-    enum PanicStatus {
+    use MacroStatus::*;
+    #[derive(Debug)]
+    enum MacroStatus {
         Panic,
+        Failure,
         Success,
     }
 
+    /// Test expected macro status against __bitpattern_testable result for some set of tokens
     macro_rules! bp_macro_test {
-        ($panic_status:ident, $($test:tt)+) => {
-            let thread_result = catch_unwind(|| __bitpattern_testable(quote!($($test)+)));
-            assert!(match $panic_status {
-                Panic => thread_result.is_err(),
-                Success => thread_result.is_ok(),
-            }, "{:?}", thread_result);
+        ($expected_status:ident, $($test:tt)+) => {
+            let macro_result = std::panic::catch_unwind(|| __bitpattern_testable(quote!($($test)+)));
+            let actual_status = match macro_result {
+                Err(_) => Panic,
+                Ok(Err(_)) => Failure,
+                Ok(Ok(_)) => Success,
+            };
+            assert!(
+                match actual_status {
+                    $expected_status => true,
+                    _ => false,
+                },
+                "__bitpattern_testable test failed: expected status {} for input `{}`. Result was `{:?}` with status {:?}.",
+                stringify!($expected_status),
+                stringify!($($test)+),
+                macro_result,
+                actual_status,
+            );
         };
     }
 
     #[test]
     fn type_too_small() {
-        bp_macro_test!(Panic, test "0b1010101010" u8);
-        bp_macro_test!(Panic, test "0b1010101010" i8);
+        bp_macro_test!(Failure, test "0b1010101010" u8);
+        bp_macro_test!(Failure, test "0b1010101010" i8);
         bp_macro_test!(Success, test "0b.....0000" u8);
         bp_macro_test!(Success, test "0b.....0000" i8);
-        bp_macro_test!(Panic,
+        bp_macro_test!(Failure,
             test
             // 129 bit pattern
             "100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
@@ -203,7 +229,7 @@ mod tests {
 
     #[test]
     fn pattern_too_long() {
-        bp_macro_test!(Panic,
+        bp_macro_test!(Failure,
             test
             // 129 bit pattern
             "100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
@@ -217,16 +243,16 @@ mod tests {
 
     #[test]
     fn invalid_pattern_chars() {
-        bp_macro_test!(Panic, test "1234");
-        bp_macro_test!(Panic, test "/\0..10.3");
+        bp_macro_test!(Failure, test "1234");
+        bp_macro_test!(Failure, test "/\0..10.3");
         bp_macro_test!(Success, test "10010");
     }
 
     #[test]
     fn wrong_literal_type() {
-        bp_macro_test!(Panic, test 0b100_01);
+        bp_macro_test!(Failure, test 0b100_01);
         bp_macro_test!(Success, test "0b100_01");
-        bp_macro_test!(Panic, test 0b10..0_01 u8);
+        bp_macro_test!(Failure, test 0b10..0_01 u8);
         bp_macro_test!(Success, test "0b10..0_01" u8);
     }
 
